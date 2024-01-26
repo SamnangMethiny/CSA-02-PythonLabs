84479611263360cb2c966cd8aaa5c3f12dc753c1
SamnangMethiny <2023518samnang@aupp.edu.kh>
2024-01-26 08:59:42 +0700

    string project

--- evalutestring.py
+++ evalutestring.py
@@ -9,8 +9,28 @@
 # "J-J" ➞ "J"
 # Notes a hyphen will separate the two letters in the string.
 
+#get user input range
+user_range= input("Enter a range of letters (e.g., a-z): ")
+
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-user_range = input("Enter a range of letters (e.g., a-z): ")
 
+#split the user's input
+first, last = user_range.split('-')
+
+
+#identify location of the start and end of the range
+firstlocation = alphabet.index(first.lower())
+lastlocation = alphabet.index(last.lower())
+
+#get range of the characters
+result_string = alphabet[firstlocation:lastlocation + 1]
+
+#identify if user input is upper or lower, if true, it will print accordingly 
+#(the false part will return 0 and so only the true part will be printed)
+result_string = first.isupper() * result_string.upper() + first.islower() * result_string.lower() 
+
+
+#Print Result string
 print(result_string)
+     
